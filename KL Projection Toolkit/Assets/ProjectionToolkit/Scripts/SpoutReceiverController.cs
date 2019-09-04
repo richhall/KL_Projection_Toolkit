f98559c9812bdfca6d8d7a2377aeea0dba5a19aa
Pete Cleary <pete@piandmash.com>
2019-09-04 17:52:22 +0100

    Created two scenes to test with

---
+++ KL Projection Toolkit/Assets/ProjectionToolkit/Scripts/SpoutReceiverController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Klak.Spout;
+
+[RequireComponent(typeof(SpoutReceiver))]
+public class SpoutReceiverController : MonoBehaviour
+{
+    public InputField inputField;
+    private SpoutReceiver _spoutReceiver;
+
+    private void OnEnable()
+    {
+        _spoutReceiver = GetComponent<SpoutReceiver>();
+        inputField.text = _spoutReceiver.nameFilter;
+    }
+
+    public void ChangeReceiver()
+    {
+        // Coroutine example
+        _spoutReceiver.nameFilter = inputField.text;
+    }
+}
